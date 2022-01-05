fe09606e56f08a404950c320df0ebae1a5832e3b
Igor Kim <igor.skh@gmail.com>
2022-01-05 16:56:22 +0100

    add progressbar component

---
+++ Shared/Components/ProgressBar.swift
@@ -0,0 +1,33 @@
+//
+//  ProgressBar.swift
+//  language_games
+//
+//  Created by Igor Kim on 31.12.21.
+//
+
+import SwiftUI
+
+struct ProgressBar: View {
+    @Binding var value: Float
+    @Binding var color: Color
+    
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack(alignment: .leading) {
+                Rectangle().frame(width: geometry.size.width , height: geometry.size.height)
+                    .opacity(0.3)
+                    .foregroundColor(Color(UIColor.systemTeal))
+                
+                Rectangle().frame(width: min(CGFloat(self.value)*geometry.size.width, geometry.size.width), height: geometry.size.height)
+                    .foregroundColor(color)
+            }.cornerRadius(45.0)
+        }
+    }
+}
+
+struct ProgressBar_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBar(value: .constant(0.5), color: .constant(Color.blue))
+            .frame(height: 40)
+    }
+}
