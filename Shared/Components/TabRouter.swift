6c2d815e96f28d2c9244eba7016d88326725a749
Igor Kim <igor.skh@gmail.com>
2022-01-17 23:19:53 +0100

    deep linking from widget

--- Shared/Components/TabRouter.swift
+++ Shared/Components/TabRouter.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import CoreData
 
 enum Page {
     case decks
@@ -42,4 +43,30 @@ class TabRouter: ObservableObject {
     @Published var isModal: Bool = false
     @Published var currentTab: Page = .decks
     @Published var selectedDeck: Deck? = nil
+    @Published var selectedCard: Card? = nil
+    @Published var addNewCard: Bool = false
+    
+    func redirectFromWidgetURL(url: URL) {
+        let urlString = url.absoluteString
+        let viewContext = PersistenceController.shared.container.viewContext
+        
+        self.addNewCard = false
+        if urlString.hasPrefix("widget://card") {
+            let urlSuffix = String(urlString.suffix(urlString.count - 14))
+            let objectURL = URL(string: urlSuffix)!
+            
+            let objectID = viewContext.persistentStoreCoordinator?.managedObjectID(forURIRepresentation: objectURL)
+            let card = viewContext.object(with: objectID!) as? Card
+            
+            self.selectedCard = card
+            self.selectedDeck = card?.decks?.sortedArray(using: []).first as? Deck
+            
+            self.currentTab = .cards
+        } else if urlString.hasPrefix("widget://newCard") {
+            self.addNewCard = true
+            self.selectedCard = nil
+            self.selectedDeck = nil
+            self.currentTab = .cards
+        }
+    }
 }
