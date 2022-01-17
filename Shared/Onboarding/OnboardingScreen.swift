8c18802449643f1698c3037e862d76844aae064d
Igor Kim <igor.skh@gmail.com>
2022-01-17 23:24:38 +0100

    update OnboardingScreen

--- Shared/Onboarding/OnboardingScreen.swift
+++ Shared/Onboarding/OnboardingScreen.swift
@@ -8,44 +8,11 @@
 import SwiftUI
 import CoreData
 
-let mockData: [MockCard] = [
-    MockCard(
-        imageName: "sample_dog",
-        translations: [
-            Translation(original: "the dog", translation: "the dog", source: .English, target: .English),
-            Translation(original: "the dog", translation: "собака", source: .English, target: .Russian),
-            Translation(original: "the dog", translation: "der Hund", source: .English, target: .German)
-        ]
-    ),
-    MockCard(
-        imageName: "sample_cat",
-        translations: [
-            Translation(original: "the cat", translation: "the cat", source: .English, target: .English),
-            Translation(original: "the cat", translation: "кошка", source: .English, target: .Russian),
-            Translation(original: "the cat", translation: "die Katze", source: .English, target: .German)
-        ]
-    ),
-    MockCard(
-        imageName: "sample_duck",
-        translations: [
-            Translation(original: "the duck", translation: "the duck", source: .English, target: .English),
-            Translation(original: "the duck", translation: "утка", source: .English, target: .Russian),
-            Translation(original: "the duck", translation: "die Ente", source: .English, target: .German)
-        ]
-    )
-]
-
-struct MockCard {
-    let imageName: String
-    let translations: [Translation]
-}
-
 class OnboardingViewModel: ObservableObject {
     private var cardsService: CardsService = .shared
     @Preference(\.firstLaunch) var firstLaunch
     
     func createSampleCards(context: NSManagedObjectContext) {
-        
         if let deck = cardsService.saveDeck(context: context, title: "SampleAnimals", onFinished: { _ in }) {
             mockData.forEach { mock in
                 guard let img = UIImage(named: mock.imageName) else { return }
@@ -54,9 +21,12 @@ class OnboardingViewModel: ObservableObject {
                 cardsService.saveCard(context: context, selectedImageData: data, card: nil, translations: mock.translations, decks: [deck]) {
                     self.firstLaunch = false
                 }
-                
             }
         }
+    }
+    
+    func skipOnboarding() {
+        firstLaunch = false
     }
 }
 
@@ -67,7 +37,7 @@ struct OnboardingScreen: View {
     var body: some View {
         Text("Creating sample data...")
             .onAppear {
-                viewModel.createSampleCards(context: viewContext)
+                viewModel.skipOnboarding()
             }
     }
 }
