3b22519bf5ed558bf110fc549f08b0ca81db8ef3
Igor Kim <igor.skh@gmail.com>
2022-02-02 00:30:41 +0100

    add mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy

--- Shared/Persistence.swift
+++ Shared/Persistence.swift
@@ -23,10 +23,13 @@ struct PersistenceController {
         storeDescription.cloudKitContainerOptions = NSPersistentCloudKitContainerOptions(containerIdentifier: "iCloud.icloud.one.beagile.polyglotflashcards")
         
         container.persistentStoreDescriptions = [storeDescription]
+        container.viewContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        container.viewContext.transactionAuthor = appTransactionAuthorName
         
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
                 AppLogger.shared.error("Unresolved PersistenceController error \(error), \(error.userInfo)")
+                
                 print("Unresolved error \(error), \(error.userInfo)")
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
